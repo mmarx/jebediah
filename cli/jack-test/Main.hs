87c072ea6d0abdedb044fb1d809173a9e4abd02a
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2015-06-07 00:51:22 +0200

    Refactor common code into Jebediah.JACK
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- cli/jack-test/Main.hs
+++ cli/jack-test/Main.hs
@@ -1,158 +1,46 @@
 module Main (main) where
 
 import Control.Applicative ((<$>))
-import Control.Concurrent ( forkIO
-                          , threadDelay
-                          )
-import Control.Concurrent.MVar
-import Control.Concurrent.STM
-import Control.Monad ( unless
-                     , when
-                     )
-import Control.Monad.Exception.Synchronous (ExceptionalT)
-import Control.Monad.Trans.Class (lift)
-import qualified Data.EventList.Absolute.TimeBody as EventList
-import Data.List (intersect)
-import Data.Maybe (fromMaybe)
-import Data.Monoid ((<>))
-import Data.Ratio ((%))
-import Foreign.C.Error (Errno)
-import System.Posix.Signals ( installHandler
-                            , keyboardSignal
-                            , Handler (Catch)
-                            )
+import Data.Default (def)
 
-import qualified Sound.MIDI.Message as MIDI
-import qualified Sound.MIDI.Message.Channel as CM
-import qualified Sound.MIDI.Message.Channel.Voice as VM
-import Sound.JACK hiding (withPort)
-import Sound.JACK.MIDI ( withPort
-                       , writeEventsToPort
-                       )
-import qualified Sound.JACK.MIDI as JM
+import qualified Sound.MIDI.Message as M
+import qualified Sound.MIDI.Message.Channel as C
+import qualified Sound.MIDI.Message.Channel.Voice as V
 
-targetPorts :: [String]
-targetPorts = [ "Bass-Station-II:midi/capture_1"
-              , "Nord-Electro-4:midi/capture_1"
-              , "MIDISPORT-2x2-Anniv:midi/capture_1"
-              , "midi-monitor:input"
-              ]
+import Jebediah.JACK hiding (at)
+import qualified Jebediah.JACK as J
 
-portRegister :: TChan PortId -> PortId -> Bool -> IO ()
-portRegister newPorts pId _ = do
-  atomically $ writeTChan newPorts pId
+targets :: [PortName]
+targets = PortName <$> [ "Bass-Station-II:midi/capture_1"
+                       , "Nord-Electro-4:midi/capture_1"
+                       , "MIDISPORT-2x2-Anniv:midi/capture_1"
+                       , "midi-monitor:input"
+                       ]
 
-withBreaking :: IO () -> IO ()
-withBreaking stuff = do
-  quitting <- newEmptyMVar
-  _ <- installHandler keyboardSignal
-       (Catch $ do
-           threadDelay 1000000
-           putMVar quitting ())
-       Nothing
-  _ <- forkIO stuff
-  _ <- takeMVar quitting
-  return ()
+nOn = M.Channel $ C.Cons { C.messageChannel = C.toChannel 1
+                         , C.messageBody = C.Voice $ V.NoteOn (V.toPitch 60) (V.toVelocity 60)
+                         }
+nOff = M.Channel $ C.Cons { C.messageChannel = C.toChannel 1
+                          , C.messageBody = C.Voice $ V.NoteOff (V.toPitch 60) (V.toVelocity 60)
+                          }
 
-handleRegistrations :: [String] -> TChan PortId -> Client -> Port typ Output -> IO ()
-handleRegistrations targets newPorts client out = do
-  pId <- atomically $ readTChan newPorts
-  port <- portById client pId
-  pn <- portName port
-  as <- portAliases port
-  connectTargets' targets client out $ pn:as
-
-connectTargets :: [String] -> Client -> Port typ Output -> String -> IO ()
-connectTargets targets client out pn = do
-  as <- portAliases =<< portByName client pn
-  connectTargets' targets client out $ pn:as
-
-connectTargets' :: [String] -> Client -> Port typ Output -> [String] -> IO ()
-connectTargets' _ _ _ [] = return ()
-connectTargets' targets client out pns = do
-  on <- portName out
-  let cns = intersect pns targets
-      want = not . null $ cns
-  when want $ do
-    mapM_ (\tn -> handleExceptions $ connect client on tn) cns
-
-type EL = EventList.T NFrames MIDI.T
-
-process :: JM.Port Output -> TQueue EL -> NFrames -> ExceptionalT Errno IO ()
-process out queue nf = do
-  mEvents <- lift $ atomically $ tryReadTQueue queue
-  writeEventsToPort out nf $ fromMaybe EventList.empty mEvents
-
-nOn = MIDI.Channel $ CM.Cons { CM.messageChannel = CM.toChannel 1
-                             , CM.messageBody = CM.Voice $ VM.NoteOn (VM.toPitch 60) (VM.toVelocity 60)
-                             }
-nOff = MIDI.Channel $ CM.Cons { CM.messageChannel = CM.toChannel 1
-                              , CM.messageBody = CM.Voice $ VM.NoteOff (VM.toPitch 60) (VM.toVelocity 60)
-                              }
-
-bpm :: Int
-bpm = 120
-
-subdivision :: Int
-subdivision = 64
-
-beatsPerMeasure :: Int
-beatsPerMeasure = 2
-
-newtype EventTime = EventTime Int
+cfg :: Config
+cfg = def { beatsPerMinute = 120
+          , beatsPerMeasure = 2
+          , targetPorts = targets
+          }
 
 at :: Int -> Int -> Int -> EventTime
-at measure beat subdiv = EventTime $ (measure - 1) * eventsPerMeasure +
-                         (beat - 1) * eventsPerBeat +
-                         (subdiv - 1)
-    where eventsPerBeat = subdivision
-          eventsPerMeasure = eventsPerBeat * beatsPerMeasure
+at m b s = J.at cfg (Measure m) (Beat b) (Subdivision s)
 
-type MEL = EventList.T EventTime MIDI.T
-
-eL :: MEL
-eL = EventList.fromPairList [ (at 1 1 1, nOn)
-                            , (at 2 1 1, nOff)
-                            , (at 3 1 1, nOn)
-                            , (at 3 2 1, nOff)
-                            , (at 3 2 16, nOn)
-                            , (at 4 1 1, nOff)
-                            ]
-
-toEventLists :: Int -> Int -> MEL -> [EL]
-toEventLists sr bs = map EventList.fromPairList .
-                     toEventLists' 0 [] [] .
-                     EventList.toPairList .
-                     EventList.mapTime (\(EventTime t) ->
-                                            NFrames $ round $ fpe * (fromIntegral t))
-    where bs' = fromIntegral bs
-          bps = bpm % 60
-          eps = bps * fromIntegral subdivision
-          fpe = (fromRational $ fromIntegral sr) / eps
-          toEventLists' _ acc cur [] = reverse $ (reverse cur):acc
-          toEventLists' off acc cur el@((NFrames time, body):rest)
-              | time <= off * bs' = toEventLists' off acc ((NFrames $ time `mod` bs', body):cur) rest
-              | otherwise = toEventLists' (off + 1) (cur:acc) [] el
-
-populateEvents :: Int -> Int -> TQueue EL -> IO ()
-populateEvents sr bs events = do
-  threadDelay 1000000
-  atomically $ mapM_ (writeTQueue events) $ toEventLists sr bs eL
+eL :: MIDIEventList
+eL = fromPairList [ (at 1 1 1, nOn)
+                  , (at 2 1 1, nOff)
+                  , (at 3 1 1, nOn)
+                  , (at 3 2 1, nOff)
+                  , (at 3 2 16, nOn)
+                  , (at 4 1 1, nOff)
+                  ]
 
 main :: IO ()
-main = do
-  putStrLn "Hello, world!"
-  handleExceptions $ withClientDefault "jebediah" $ \client -> do
-    (newPorts, events) <- lift $ do
-      s <- getSampleRate client
-      b <- getBufferSize client
-      n <- newTChanIO
-      e <- newTQueueIO
-      _ <- forkIO $ populateEvents s b e
-      return $ (n, e)
-    withPortRegistration client (portRegister newPorts) $
-      withPort client "midi" $ \out ->
-         withProcess client (process out events) $
-           withActivation client $ lift $ do
-             getPorts client >>= mapM_ (connectTargets targetPorts client out)
-             withBreaking $ handleRegistrations targetPorts newPorts client out
+main = putStrLn "Hello, world!" >> jebediahMain cfg eL
---
+++ src/Jebediah/JACK.hs
@@ -0,0 +1,169 @@
+module Jebediah.JACK
+    ( Config (..)
+    , EventTime (EventTime)
+    , MIDIEventList
+    , Measure (Measure)
+    , Beat (Beat)
+    , Subdivision (Subdivision)
+    , PortName (PortName)
+    , at
+    , EL.fromPairList
+    , jebediahMain
+    ) where
+
+import Control.Applicative ((<$>))
+import Control.Concurrent ( forkIO
+                          , threadDelay
+                          )
+import Control.Concurrent.MVar ( newEmptyMVar
+                               , putMVar
+                               , takeMVar
+                               )
+import Control.Concurrent.STM ( TChan
+                              , TQueue
+                              , atomically
+                              , newTChanIO
+                              , newTQueueIO
+                              , readTChan
+                              , tryReadTQueue
+                              , writeTChan
+                              , writeTQueue
+                              )
+import Control.Monad (when)
+import Control.Monad.Exception.Synchronous (ExceptionalT)
+import Control.Monad.Trans.Class (lift)
+import Data.Default (Default, def)
+import qualified Data.EventList.Absolute.TimeBody as EL
+import Data.List (intersect)
+import Data.Maybe (fromMaybe)
+import Data.Ratio ((%))
+import Foreign.C.Error (Errno)
+import qualified Sound.MIDI.Message as M
+import Sound.JACK hiding (withPort)
+import Sound.JACK.MIDI ( withPort
+                       , writeEventsToPort
+                       )
+import qualified Sound.JACK.MIDI as JM
+import System.Posix.Signals ( installHandler
+                            , keyboardSignal
+                            , Handler (Catch)
+                            )
+
+newtype PortName = PortName { unPortName :: String }
+newtype SampleRate = SampleRate { unSampleRate :: Int }
+newtype BufferSize = BufferSize { unBufferSize :: Int }
+newtype EventTime = EventTime { unEventTime :: Int }
+newtype Measure = Measure { unMeasure :: Int }
+newtype Beat = Beat { unBeat :: Int }
+newtype Subdivision = Subdivision { unSubdivision :: Int }
+type EventList = EL.T NFrames M.T
+type MIDIEventList = EL.T EventTime M.T
+
+
+portRegister :: TChan PortId -> PortId -> Bool -> IO ()
+portRegister newPorts pId _ = do
+  atomically $ writeTChan newPorts pId
+
+withBreaking :: IO () -> IO ()
+withBreaking stuff = do
+  quitting <- newEmptyMVar
+  _ <- installHandler keyboardSignal
+       (Catch $ do
+           threadDelay 1000000
+           putMVar quitting ())
+       Nothing
+  _ <- forkIO stuff
+  _ <- takeMVar quitting
+  return ()
+
+handleRegistrations :: [PortName] -> TChan PortId -> Client -> Port typ Output -> IO ()
+handleRegistrations targets newPorts client out = do
+  pId <- atomically $ readTChan newPorts
+  port <- portById client pId
+  pn <- portName port
+  as <- portAliases port
+  connectTargets' targets client out $ pn:as
+
+connectTargets :: [PortName] -> Client -> Port typ Output -> String -> IO ()
+connectTargets targets client out pn = do
+  as <- portAliases =<< portByName client pn
+  connectTargets' targets client out $ pn:as
+
+connectTargets' :: [PortName] -> Client -> Port typ Output -> [String] -> IO ()
+connectTargets' _ _ _ [] = return ()
+connectTargets' targets client out pns = do
+  on <- portName out
+  let cns = intersect pns $ unPortName <$> targets
+      want = not . null $ cns
+  when want $ do
+    mapM_ (\tn -> handleExceptions $ connect client on tn) cns
+
+process :: JM.Port Output -> TQueue EventList -> NFrames -> ExceptionalT Errno IO ()
+process out queue nf = do
+  mEvents <- lift $ atomically $ tryReadTQueue queue
+  writeEventsToPort out nf $ fromMaybe EL.empty mEvents
+
+toEventLists :: Config -> SampleRate -> BufferSize -> MIDIEventList -> [EventList]
+toEventLists cfg sr bs = map EL.fromPairList .
+                         toEventLists' 0 [] [] .
+                         EL.toPairList .
+                         EL.mapTime (\t -> NFrames $ round $
+                                     fpe * (fromIntegral $ unEventTime t))
+    where bs' = fromIntegral $ unBufferSize bs
+          bpm = beatsPerMinute cfg
+          bps = bpm % 60
+          eps = bps * fromIntegral (subdivisions cfg)
+          fpe = (fromRational $ fromIntegral $ unSampleRate sr) / eps
+          toEventLists' _ acc cur [] = reverse $ (reverse cur):acc
+          toEventLists' off acc cur el@((NFrames time, body):rest)
+              | time <= off * bs' = toEventLists' off acc ((NFrames $ time `mod` bs', body):cur) rest
+              | otherwise = toEventLists' (off + 1) (cur:acc) [] el
+
+data Config = Config { beatsPerMinute :: Int
+                     , beatsPerMeasure :: Int
+                     , subdivisions :: Int
+                     , targetPorts :: [PortName]
+                     , startupDelay :: Int
+                     , clientName :: String
+                     , outName :: String
+                     }
+
+instance Default Config
+    where def = Config { beatsPerMinute = 120
+                       , beatsPerMeasure = 4
+                       , subdivisions = 64
+                       , targetPorts = []
+                       , startupDelay = 1000000
+                       , clientName = "jebediah"
+                       , outName = "midi"
+                       }
+
+populateEvents :: Config -> SampleRate -> BufferSize -> MIDIEventList -> TQueue EventList -> IO ()
+populateEvents cfg sr bs el events = do
+  threadDelay $ startupDelay cfg
+  atomically $ mapM_ (writeTQueue events) $ toEventLists cfg sr bs el
+
+at :: Config -> Measure -> Beat -> Subdivision -> EventTime
+at cfg measure beat subdiv = EventTime $ ((unMeasure measure) - 1) * eventsPerMeasure +
+                             ((unBeat beat) - 1) * eventsPerBeat +
+                             ((unSubdivision subdiv) - 1)
+    where eventsPerBeat = subdivisions cfg
+          eventsPerMeasure = eventsPerBeat * beatsPerMeasure cfg
+
+jebediahMain :: Config -> MIDIEventList -> IO ()
+jebediahMain cfg el = do
+  let tgts = targetPorts cfg
+  handleExceptions $ withClientDefault (clientName cfg) $ \client -> do
+    (newPorts, events) <- lift $ do
+      s <- SampleRate <$> getSampleRate client
+      b <- BufferSize <$> getBufferSize client
+      n <- newTChanIO
+      e <- newTQueueIO
+      _ <- forkIO $ populateEvents cfg s b el e
+      return $ (n, e)
+    withPortRegistration client (portRegister newPorts) $
+      withPort client (outName cfg) $ \out ->
+         withProcess client (process out events) $
+           withActivation client $ lift $ do
+             getPorts client >>= mapM_ (connectTargets tgts client out)
+             withBreaking $ handleRegistrations tgts newPorts client out
